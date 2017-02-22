58a1179b67eb72a2389cca5b140994be74c2e329
EugeneSqr <eskurihin@gmail.com>
2017-02-22 17:39:16 +0300

    setup fonts removed name duplication

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -3,18 +3,20 @@ source ./setup-utility.sh
 
 progress "setup powerline fonts"
 fontDir=~/.local/share/fonts
+fontName=PowerlineSymbols.otf
 mkdir -p $fontDir
-if [ ! -f $fontDir/PowerlineSymbols.otf ]; then
-    wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf -qP $fontDir
+if [ ! -f $fontDir/$fontName ]; then
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/$fontName -qP $fontDir
 
     fc-cache -f -v
 fi
 
 fontConfigDir=~/.config/fontconfig/fonts.conf
+fontConfigName=10-powerline-symbols.conf
 mkdir -p $fontConfigDir
 
-if [ ! -f $fontConfigDir/10-powerline-symbols.conf ]; then
-    wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf -qP $fontConfigDir
+if [ ! -f $fontConfigDir/$fontConfigName ]; then
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/$fontConfigName -qP $fontConfigDir
 fi
 
 pass "setup powerline fonts"
